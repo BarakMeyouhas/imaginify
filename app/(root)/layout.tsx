1f20988f453fbb79ad5672c338de77c2bf8b024f
Barak1998 <barakm25@gmail.com>
2024-03-30 15:03:37 +0300

    finished project

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,20 +1,22 @@
-import MobileNav from "@/components/shared/MobileNav";
-import Sidebar from "@/components/shared/Sidebar";
-import { Toaster } from "@/components/ui/toaster";
+import MobileNav from '@/components/shared/MobileNav'
+import Sidebar from '@/components/shared/Sidebar'
+import { Toaster } from '@/components/ui/toaster'
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <main className="root">
+    <main className='flex min-h-screen w-full bg-white'>
       <Sidebar />
       <MobileNav />
 
-      <div className="root-container">
-        <div className="wrapper">{children}</div>
+      <div className='mt-16 flex-1 overflow-auto py-8'>
+        <div className='max-w-5xl mx-auto px-5 text-dark-400 p-16-regular'>
+          {children}
+        </div>
       </div>
-
+      
       <Toaster />
     </main>
-  );
-};
+  )
+}
 
-export default Layout;
+export default Layout
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,9 @@
 import type { Metadata } from "next";
-import { IBM_Plex_Sans, Inter } from "next/font/google";
-import "./globals.css";
+import { IBM_Plex_Sans } from "next/font/google";
 import { cn } from "@/lib/utils";
 import { ClerkProvider } from "@clerk/nextjs";
 
-const inter = Inter({ subsets: ["latin"] });
+import "./globals.css";
 
 const IBMPlex = IBM_Plex_Sans({
   subsets: ["latin"],
@@ -23,11 +22,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider appearance={{
-      variables: {
-        colorPrimary: "#624cf5",
-      },
-    }}>
+    <ClerkProvider
+      appearance={{
+        variables: { colorPrimary: "#624cf5" },
+      }}
+    >
       <html lang="en">
         <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>
           {children}
